6b230c87a3a9dbd844ef32bbe2468ba03a4b8585
Yellrus <ruslan.namanov@yandex.ru>
2016-05-18 04:51:10 +0300

    Готово

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,29 +1,38 @@
-$(document).ready(function(){
+// Функция срабатывает по событию .blur
+    $('#phoneNumber').blur(function(){
+        // Убираем все классы с поля «Ваш номер телефона»
+        $('#phoneNumber').removeClass();
+        // Берем значение поля «Ваш номер телефона»
+        var phoneV = $('#phoneNumber').val();
+        // Определяем длину значения поля
+        var phoneLngth = phoneV.length;
 
-    $(".form-yellow").validate({
-
-       rules:{
-
-            phoneNumber:{
-                required: true,
-                number: true,
-                minlength: 10,
-            },
-
-       },
-
-       messages:{
-
-            phoneNumber:{
-                required: "Это поле обязательно для заполнения",
-                number: "Укажите правильный номер",
-                minlength: "Укажите правильный номер",
-            },
-
-
-
-       }
-
+        // Проверяем чтобы в поле были только цифры
+        if( /[^0-9]/.test(phoneV) ) {
+            $('#phoneNumber').addClass('notValid');
+            $('#spnPhoneStatus').css('color', 'red');
+            $(this).next().text('Укажите правильный телефон');
+        } else if (phoneLngth <= 5) {
+        // Проверяем чтобы длина номера телефона была не меньше 6 символов
+            $('#phoneNumber').addClass('notValid');
+            $('#spnPhoneStatus').css('color', 'red');
+            $(this).next().text('Укажите правильный телефон');
+        } 
     });
 
-});
+
+
+// тут у нас Мини-автоподбор слов
+
+    $(function() {
+        var availableTags = [
+          "Проворный ткачик",
+          "ПРОФИ-СПОРТ",
+          "Проспект",
+          "Просто Чудо",
+          "Прокат Пони+",
+        ];
+        $( "#franchiseName" ).autocomplete({
+          source: availableTags
+        });
+      });
